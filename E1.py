b55771747feb5eb62c3f06dbae17b85d241b5e6b
Mohit <mhtkumar46@gmail.com>
2020-12-20 16:56:17 +0530

    new

---
+++ E1.py
@@ -0,0 +1,3 @@
+radius = input('Enter the radius of circle: ')
+area = 3.14 * int(radius) ** 2
+print('Area of circle is', area)
---
+++ e2.py
@@ -0,0 +1,8 @@
+num1 = 37.467634864
+num2 = 46.467867335
+
+# print('num 1 is', num1,'and num 2 is', num2)
+
+# print('num 1 is {0:.2f} and num 2 is {1:.2f}'.format(num1, num2))
+
+print(f'num 1 is {num1:.2f} and num 2 is {num2:.2f}')
